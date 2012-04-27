81c7b7219a783803cfc84060aa204a434cc3805f
Gil Fink <GILF@GILF-PC.(none)>
2012-04-27 16:22:59 +0300

    New features: query and change tracker

---
+++ story.js/story.changeTracker.js
@@ -0,0 +1,23 @@
+﻿/*!
+* story.js v0.1
+*
+* Copyright (c) Sela Group and Gil Fink. All rights reserved.
+* Dual licensed under the MIT or GPL Version 2 licenses.
+*
+* Date: Fri Apr 20 14:00:12 2012 
+*/
+
+/*
+* story.js enables web developers to use client-side storages in a common and simple way. 
+* It includes abstraction layer on top of Web Storage API, IndexedDB API, Cookies API and 
+* In-Memory storage.
+*
+* Authors        Gil Fink
+* Contributors   Ran Wahle
+*/
+
+(function (story) {
+
+
+
+} (this.story = this.story || {}));
